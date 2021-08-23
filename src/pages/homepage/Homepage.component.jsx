e5a9c2ca60b02e4af54b4f3d5292cfbec82f3bcc
Dmitrij <dmitrij.jakush@gmail.com>
2021-08-23 22:48:59 +0300

    added some new components,styling

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory.component';
+import './homepage.styles.scss';
+
+export const Homepage = () => {
+  return <Directory />;
+};
